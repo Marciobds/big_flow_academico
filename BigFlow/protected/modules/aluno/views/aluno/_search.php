75917b3b87b5bf0e05afe0e63a1cc14373c98b5f
Marcio Bortolini dos Santos <Marciobds@marciobds.localdomain>
2014-05-09 18:42:42 -0300

    Projeto criado

---
+++ BigFlow/protected/modules/aluno/views/aluno/_search.php
@@ -0,0 +1,35 @@
+<?php
+/* @var $this AlunoController */
+/* @var $model Aluno */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'id'); ?>
+		<?php echo $form->textField($model,'id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'nome'); ?>
+		<?php echo $form->textField($model,'nome',array('size'=>60,'maxlength'=>100)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'matricula'); ?>
+		<?php echo $form->textField($model,'matricula',array('size'=>45,'maxlength'=>45)); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
